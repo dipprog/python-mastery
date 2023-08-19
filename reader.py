ce9587b37b3562b9a040f936704ddebed214b21e
Dip Kumar Das <dipkumardasdh@gmail.com>
2023-08-20 01:10:50 +0530

    exercise 3.1

---
+++ reader.py
@@ -0,0 +1,61 @@
+# reader.py
+
+import csv
+
+def read_csv_as_dicts(filename, types):
+    '''
+    Read a CSV file with column type conversion
+    '''
+    records = []
+    with open(filename, 'r') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            record = {name: func(val) for name, func, val in zip(headers, types, row)}
+            records.append(record)
+
+    return records
+
+from collections.abc import Sequence
+class DataCollection(Sequence):
+    def __init__(self, headers):
+        for col_name in headers:
+            setattr(self, col_name, [])
+        self.headers = headers
+    
+    def __len__(self):
+        return len(getattr(self, self.headers[0]))
+    
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            new_self = self.__class__(self.headers)
+            for col_name in self.headers:
+                setattr(new_self, col_name, getattr(self, col_name)[index])
+            return new_self
+        
+        data = {col_name:getattr(self, col_name)[index] for col_name in self.headers}
+        return data
+    
+    def append(self, d):
+        for col_name in self.headers:
+            getattr(self, col_name).append(d[col_name])
+
+
+def read_csv_as_columns(filename, types):
+    with open(filename, 'r') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        data = DataCollection(headers)
+        for row in rows:
+            record = {name:func(val) for name, func, val in zip(headers, types, row)}
+            data.append(record)
+
+    return data
+
+
+
+            
+        
+
+
+    
--- stock.py
+++ stock.py
@@ -8,4 +8,28 @@ class Stock:
 
     def cost(self):
         return self.shares * self.price
+
+    def sell(self, nshares):
+        if nshares > 0 and nshares < self.shares:
+            self.shares -= nshares
+
+import csv
+def read_portfolio(filename):
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)    # Skip headers
+        for row in rows:
+            records.append( Stock( str(row[0]), int(row[1]),float(row[2]) ) )
+    return records
+
+def print_portfolio(portfolio):
+    headers = ['name', 'shares', 'price']
+    print('%10s %10s %10s' % (headers[0], headers[1], headers[2]))
+    print(('-' * 10 + ' ') * len(headers))
+    for s in portfolio:
+        print('%10s %10d %10.2f' % (s.name, s.shares, s.price))
+
+
+        
         
