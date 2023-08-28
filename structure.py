be7ba859affa105fa82c076385138797c30171cd
Dip Kumar Das <dipkumardasdh@gmail.com>
2023-08-28 13:24:23 +0530

    exercise 6.2

--- structure.py
+++ structure.py
@@ -1,12 +1,15 @@
 # structure.py
 
+import sys
+
 class Structure:
     _fields = tuple()
-    def __init__(self, *args):
-        if len(args) != len(self._fields):
-            raise TypeError('Expected %d arguments' % len(self._fields))
-        for name, arg in zip(self._fields, args):
-            setattr(self, name, arg)
+    @staticmethod
+    def _init():
+        locs = sys._getframe(1).f_locals
+        self = locs.pop('self')
+        for name, val in locs.items():
+            setattr(self, name, val)
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}({', '.join(repr(getattr(self, name)) for name in self._fields)})"
@@ -16,9 +19,3 @@ class Structure:
             super().__setattr__(name, value)
         else:
             raise AttributeError('No attribute %s' % name)
-
-class Stock(Structure):
-    _fields = ('name', 'shares', 'price')
-
-class Date(Structure):
-    _fields = ('year', 'month', 'day')
